e01afef8bfa78368d43d49ece4e27564d379247c
jukanntenn <jukanntenn@outlook.com>
2021-12-21 22:00:15 +0800

    Implemented `TreeComment` abstract model

---
+++ tree_comments/models.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from tree_queries.models import TreeNode
+
+
+class TreeComment(TreeNode):
+    content = models.TextField(_("content"))
+    created_at = models.DateTimeField(_("created at"), default=None)
+    ip_address = models.GenericIPAddressField(
+        _("IP address"), unpack_ipv4=True, blank=True, null=True
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("user"),
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        abstract = True
+        verbose_name = _("tree comment")
+        verbose_name_plural = _("tree comments")
+
+    def __str__(self):
+        return "%s: %s..." % (self.user, self.content[:50])
+
+    def save(self, *args, **kwargs):
+        if self.created_at is None:
+            self.created_at = timezone.now()
+        super().save(*args, **kwargs)
